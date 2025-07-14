fb27b356db75bbafef0869df3b4ee753760f78e3
Junkun Yuan <yuanjk0921@outlook.com>
2025-07-14 19:20:15 +0800

    add Lumos-1

--- paper_reading_list/resource/visual_generation.py
+++ paper_reading_list/resource/visual_generation.py
@@ -25,6 +25,34 @@ VISUAL_GENERATION["papers"] = [
 # """,
 # },
 {
+"title": "Lumos-1: On Autoregressive Video Generation from a Unified Model Perspective",
+"author": "Hangjie Yuan, Weihua Chen, Jun Cen, Hu Yu, Jingyun Liang, Shuning Chang, Zhihui Lin, Tao Feng, Pengwei Liu, Jiazheng Xing, Hao Luo, Jiasheng Tang, Fan Wang, Yi Yang",
+"organization": "DAMO Academy, Alibaba Group, Hupan Lab, Zhejiang University, Tsinghua University",
+"date": "20250711",
+"venue": "arXiv 2025",
+"pdf_url": "https://arxiv.org/pdf/2507.08801",
+"code_url": "https://github.com/alibaba-damo-academy/Lumos",
+"name": "Lumos-1",
+"comment": "",
+"category": "Foundation Algorithms & Models",
+"jupyter_notes": "",
+"summary": """It employs LLM architecture to achieve auto-regressive video generation with some improvement on RoPE and masking strategy.""",
+"details": 
+"""
+<ul>
+    <li><b>Structure:</b> Llama with a new RoPE strategy to model multimodal spatiotemporal dependency.
+    <li><b>Tokenizer:</b> Cosmos's visual tokenizer with spatiotemporal compression rates of 8x8x4; Chameleon's text encoder.
+    <li> <b>Model size:</b> 0.5B, 1B, and 3B.
+</ul>
+fig: fig1.png 550
+cap: <b>Text-to-image</b> generation performance on <b>GenEval</b>.
+fig: fig2.png 650
+cap: <b>Image-to-video</b> generation performance on <b>VBench-I2V</b>.
+fig: fig3.png 650
+cap: <b>Text-to-video</b> generation performance on <b>VBench-T2V</b>.
+""",
+},
+{
 "title": "MAGI-1: Autoregressive Video Generation at Scale",
 "author": "Hansi Teng, Hongyu Jia, Lei Sun, Lingzhi Li, Maolin Li, Mingqiu Tang, Shuai Han, Tianning Zhang, W.Q. Zhang, Weifeng Luo, Xiaoyang Kang, Yuchen Sun, Yue Cao, Yunpeng Huang, Yutong Lin, Yuxin Fang, Zewei Tao, Zheng Zhang, Zhongshu Wang, Zixun Liu, Dai Shi, Guoli Su, Hanwen Sun, Hong Pan, Jie Wang, Jiexin Sheng, Min Cui, Min Hu, Ming Yan, Shucheng Yin, Siran Zhang, Tingting Liu, Xianping Yin, Xiaoyu Yang, Xin Song, Xuan Hu, Yankai Zhang, Yuqiao Li",
 "organization": "Sand AI",
